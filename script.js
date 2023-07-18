8feb7e698418a3ddffd25faea495e967cbdfcc4d
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-07-18 19:58:27 +0300

    [script] Mose Interactivity

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ canvas.height = 500;
 let score = 0;
 let gameFrame = 0;
 ctx.font = "50px Georgia";
+
 // Mose Interactivity
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+    x: canvas.width/2,
+    y: canvas.width/2,
+    click: false
+}
+canvas.addEventListener("mousedown", function(event) {
+    mouse.x = event.x - canvasPosition.left;
+    mouse.x = event.y - canvasPosition.top;
+});
 // Player
 // Bubbles
 // Animation Loop
