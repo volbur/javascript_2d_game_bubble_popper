a85a7e43354d9620562a6a477fed7e03c91d67a3
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-07-18 10:22:59 +0300

    [script] Canvas setup

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Canvas setup
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = "50px Georgia";
 // Mose Interactivity
 // Player
 // Bubbles
