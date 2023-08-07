faef1b3bb8b58353f5897dd0a7fe7ef0adb8239d
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-08-07 19:21:50 +0300

    Addid animation for enemy

--- script.js
+++ script.js
@@ -194,6 +194,19 @@ class Enemy {
             this.y = Math.random() * (canvas.height - 150) + 90;
             this.speed = Math.random() * 2 + 2;
         }
+        if (gameFrame % 5 == 0) {
+            this.frame++;
+            if (this.frame >= 12) this.frame = 0;
+            if (this.frame == 3 || this.frame == 7 || this.frame == 11) {
+                this.frameX = 0;
+            } else {
+                this.frameX++;
+            }
+            if (this.frame < 3) this.frameY = 0;
+            else if (this.frame < 7) this.frameY = 1;
+            else if (this.frame < 11) this.frameY = 2;
+            else this.frameY = 0;
+        }
     }
 }
 const enemy1 = new Enemy();
